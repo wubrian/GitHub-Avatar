d7453c52d1be17f22626b8528df48b8c0d9809dd
Brian Wu <wubrian1234@hotmail.com>
2018-12-04 02:09:52 +0000

    hide tokens and modify .gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,21 @@
 var request = require('request');
+var tokens = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': "token " + tokens.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    console.log(res.headers);
     cb(err, body);
   });
-
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
